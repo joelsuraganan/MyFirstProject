48861be4682346819e64f12131c2223b8bfd2698
joelsuraganan <131628719+joelsuraganan@users.noreply.github.com>
2025-03-23 13:39:34 -0400

    Delete random_number.py
    duplicate

--- random_number.py
+++
@@ -1,8 +0,0 @@
-import random
-
-def generate_random_number():
-    """Returns a random number between 1 and 100."""
-    return random.randint(1, 100)
-
-if __name__ == "__main__":
-    print("Random number:", generate_random_number())
